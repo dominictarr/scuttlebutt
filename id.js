a404ae6197280f7c92a81b7ad7b079a462da6053
Raynos <raynos2@gmail.com>
2012-08-29 22:08:20 -0700

    no more dead code

--- id.js
+++
@@ -1,9 +0,0 @@
-var map = require('iterate').map
-
-module.exports = 
-function createID () {
-  return map(3, function (i) {
-    return Math.random().toString(16).substring(2).toUpperCase()
-  }).join('')
-}
-
