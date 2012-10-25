e6419829a83da546d414b982282fab7f85ace1c2
Dominic Tarr <dominic.tarr@gmail.com>
2012-10-25 14:58:33 +0200

    test for dispose

---
+++ test/dispose.js
@@ -0,0 +1,40 @@
+
+/*
+we need to be able to stream the state of the model to disk,
+and then call doc.dispose()
+and end the stream.
+*/
+
+var EE = require('../events')
+var assert = require('assert')
+var es = require('event-stream')
+var mac = require('macgyver')()
+process.on('exit', mac.validate)
+
+var emitter = new EE()
+var ended = false
+
+var fs = require('fs')
+
+var s = emitter.createReadStream({tail: true})
+  s.on('readable', function () {
+    console.log('READABLE')
+  })
+//  s.pipe(fs.createWriteStream('/tmp/dispose-test'))
+  s.pipe(es.writeArray(function (err, ary) {
+    console.log(ary)
+    ended = true
+  }))
+  
+emitter.emit('message', 'hello')
+emitter.emit('message', 'hello')
+
+emitter.on('dispose', function () {
+  console.log('DISPOSE')
+})
+
+process.nextTick(function () {
+  console.log('dispose')
+  emitter.dispose()
+  assert(ended, 'dispose must trigger end on all streams')
+})
