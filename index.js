2c6ebd9cbc98ef9df8b56eca0847d1db2d4ae0e4
Dominic Tarr <dominic.tarr@gmail.com>
2012-12-05 15:56:34 -0800

    use only one stream function

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 //really simple data replication.
 
 var EventEmitter = require('events').EventEmitter
@@ -26,12 +25,9 @@ function validate (data) {
   var ts = data[1], source = data[2]
 
   if(  !Array.isArray(data) 
-//    || data.length < 4
-//    || 'string'    !== typeof key
     || 'string'    !== typeof source
     || 'number'    !== typeof ts
-  ) 
-    return false
+  ) return false
 
   return true
 }
@@ -137,41 +133,52 @@ sb.createStream = function (opts) {
 
   var syncSent = false, syncRecv = false
 
+  opts = opts || {}
   var d = duplex()
-  d.name = opts && opts.name
+  d.name = opts.name
   var outer = serializer(opts && opts.wrapper)(d)
   outer.inner = d
+
+  d.writable = opts.writable !== false
+  d.readable = opts.readable !== false
+
+  var tail = opts.tail !== false //default to tail=true
+
+  function start (data) {
+    //when the digest is recieved from the other end,
+    //send the history.
+    //merge with the current list of sources.
+    sources = data.clock
+    i.each(self.history(sources), d._data.bind(d))
+    console.log('start', self.history(sources))
+    
+    outer.emit('header', data)
+    d._data('SYNC')
+    //when we have sent all history
+    outer.emit('sync')
+    syncSent = true
+    //when we have recieved all histoyr
+    //emit 'synced' when this stream has synced.
+    if(syncRecv) outer.emit('synced')
+    if(!tail) d._end()
+  }
+
   d
-    .on('write', function (data) {
+    .on('_data', function (data) {
     //if it's an array, it's an update.
-    //if it's an object, it's a scuttlebut digest.
       if(Array.isArray(data)) {
         if(validate(data))
           return self._update(data)
       }
-      //suppose we want to disconnect after we have synced?
-      else if('object' === typeof data && data) {
-        //when the digest is recieved from the other end,
-        //send the history.
-        //merge with the current list of sources.
-        sources = data.clock
-        i.each(self.history(sources), d.emitData.bind(d))
-
-        outer.emit('header', data)
-        d.emitData('SYNC')
-        //when we have sent all history
-        outer.emit('sync')
-        syncSent = true
-        //when we have recieved all histoyr
-        //emit 'synced' when this stream has synced.
-        if(syncRecv) outer.emit('synced')
-      }
+      //if it's an object, it's a scuttlebut digest.
+      else if('object' === typeof data && data)
+        start(data)
       else if('string' === typeof data && data == 'SYNC') {
         syncRecv = true
         if(syncSent) outer.emit('synced')
       }
-    }).on('ended', function () {
-      d.emitEnd()
+    }).on('_end', function () {
+      d._end()
     })
     .on('close', function () {
       self.removeListener('_update', onUpdate)
@@ -180,7 +187,7 @@ sb.createStream = function (opts) {
   if(opts && opts.tail === false) {
     outer.on('sync', function () {
       process.nextTick(function () {
-        d.emitEnd()
+        d._end()
       })
     })
   }
@@ -189,7 +196,7 @@ sb.createStream = function (opts) {
       return
 
     //if I put this after source[source]= ... it breaks tests
-    d.emitData(update)
+    d._data(update)
 
     //really, this should happen before emitting.
     var ts = update[1]
@@ -198,105 +205,34 @@ sb.createStream = function (opts) {
   }
 
   var outgoing = { id : self.id, clock : self.sources }
+
   if (opts && opts.meta) outgoing.meta = opts.meta
-  d.emitData(outgoing)
- 
-  self.on('_update', onUpdate)
-  return outer
-}
 
-//createReadStream -- for persisting.
-
-sb.createReadStream = function (opts) {
-  opts = opts || {}
-
-  //write this.id
-  //then write the histroy.
-  //then, if opts.tail
-  //listen for updates, else emit 'end'
-
-  var out = this.history()
-  out.unshift(this.id)
-
-  var tail = opts.tail !== false //default to tailing
-
-  var wrapper = ({
-    json: function (e) { return JSON.stringify(e) + '\n' },
-    raw: function (e) { return e }
-  }) [opts.wrapper || 'json']
-
-  var rs = new ReadableStream()
-  rs.read = function () {
-    var data = out.shift()
-
-    if(!data && !tail) {
-      return this.emit('end'), null
-    }
-    if(!data)
-      return null
-    
-    return wrapper(data)
+  if(d.readable) {
+    d._data(outgoing)
+    if(!d.writable)
+      start({clock:{}})
+    if(tail)
+      self.on('_update', onUpdate)
   }
 
-  rs.end = function () {
-    tail = false
-    rs.emit('readable')    
-    process.nextTick(rs.destroy)
-  }
-
-  function onUpdate (update) {
-    out.push(update)
-    rs.emit('readable')
-  }
-
-  if(tail) {
-    this.on('_update', onUpdate)
-  }
-  var self = this
-  rs.destroy = function () {
-    rs.removeListener('_update', onUpdate)
-    rs.emit('close')
-    //should this emit end?
-    //this is basically close,
-    //does readable-stream actually support this?
-    return rs
-  }
-
-  this.once('dispose', function () {
-    tail = false //close the stream soon.
-    rs.end()
+  self.once('dispose', function () {
+    d._end()
   })
 
-  return rs
+  return outer
 }
 
 sb.createWriteStream = function (opts) {
   opts = opts || {}
-  var Stream = require('stream')
-  var ws = new Stream()
-  var self = this, first = true
-  ws.writable = true
-  ws.write = function (data) {
-    if(!this.writable) return
-    if('string' === typeof data)
-      return self.id = data, true
-    first = false
-    self._update(data)
-    return true
-  }
-  ws.end = function () {
-    this.writable = false
-    self.sync = true
-    //there are probably bugs in persisting when there are lots of streams.
-    //TODO: figure out what they are! then fix them!
-    emit.call(self, 'sync')
-    process.nextTick(ws.destroy.bind(ws))
-  }
-  ws.destroy = function () {
-    this.writable = false
-    this.emit('close')
-  }
-  return serializer(opts.wrapper)(ws)
+  opts.writable = true; opts.readable = false
+  return this.createStream(opts)
+}
+
+sb.createReadStream = function (opts) {
+  opts = opts || {}
+  opts.writable = false; opts.readable = true
+  return this.createStream(opts)
 }
 
 sb.dispose = function () {
