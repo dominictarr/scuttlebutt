17b88b54c379d521ad1f580b6ad99d250b9c27a7
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-04 14:24:21 +0700

    test for cloning scuttlebutt instances

---
+++ test/clone.js
@@ -0,0 +1,34 @@
+
+var Model = require('../model')
+var tape = require('tape')
+
+/*tape('clone() -> another instance', function (t) {
+  var a = new Model()
+  var b = a.clone()
+  t.equal(b.constructor, Model)
+  t.end()
+})*/
+
+tape('clone() -> deepEqual history', function (t) {
+  var a = new Model()
+  a.set('foo', 'bar')
+  var b = a.clone()
+
+  t.deepEqual(b.history(), a.history())
+  t.end()
+
+})
+
+tape('clone() -> updates apply to both instances', function (t) {
+  var a = new Model()
+  a.set('foo', 'bar')
+
+  var b = a.clone()
+  t.deepEqual(b.history(), a.history())
+
+  b.set('quux', 'zaff')
+  t.deepEqual(b.history(), a.history())
+
+  t.end()
+})
+
