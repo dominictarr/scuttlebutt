e8d3a50db6c5563d4df78722a1138b3972bb97ad
Dominic Tarr <dominic.tarr@gmail.com>
2013-01-04 17:37:47 +0700

    every test must .end()

--- test/model.js
+++ test/model.js
@@ -11,12 +11,20 @@ var expected = {
 a.on('change', mac(function (key, value) {
   t.ok(expected[key] !== undefined)
   t.equal(value, expected[key])
+  next()
 }).atLeast(1))
 
 a.on('change:key', mac(function (value) {
   t.equal(value, expected.key)
+  next()
 }).once())
 
 a.set('key', expected.key)
+
+  var n = 2
+  function next () {
+    if(--n) return
+    t.end()
+  }
 
 })
