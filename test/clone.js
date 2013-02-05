ff241a4c976e03561dca1c7223946d19803e521b
Dominic Tarr <dominic.tarr@gmail.com>
2013-02-05 14:16:31 +1300

    tests for unclone

--- test/clone.js
+++ test/clone.js
@@ -32,3 +32,27 @@ tape('clone() -> updates apply to both instances', function (t) {
   t.end()
 })
 
+
+tape('clone() -> dispose triggers unclone event', function (t) {
+  var a = new Model(), uncloned = false
+  a.set('foo', 'bar')
+
+  var b = a.clone()
+  t.deepEqual(b.history(), a.history())
+
+  t.equal(a._clones, 1)
+
+  b.set('quux', 'zaff')
+  t.deepEqual(b.history(), a.history())
+  
+  a.on('unclone', function (clones) {
+    uncloned = true
+    t.equal(clones, 0, 'should have zero clones')
+  })
+
+  b.dispose()
+
+  t.equal(uncloned, true)
+  t.equal(a._clones, 0)
+  t.end()
+})
