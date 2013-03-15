0d0988cc2a12327e30574ae96119c23fa03330ad
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-15 19:50:17 +1300

    test Model.toJSON()

--- test/model.js
+++ test/model.js
@@ -8,23 +8,34 @@ var expected = {
   key: Math.random()
 }
 
-a.on('change', mac(function (key, value) {
+t.deepEqual({}, a.toJSON())
+
+a.once('change', mac(function (key, value) {
   t.ok(expected[key] !== undefined)
   t.equal(value, expected[key])
+  t.deepEqual(a.toJSON(), expected)
   next()
 }).atLeast(1))
 
-a.on('change:key', mac(function (value) {
+a.once('change:key', mac(function (value) {
   t.equal(value, expected.key)
   next()
 }).once())
 
-a.set('key', expected.key)
 
   var n = 2
   function next () {
     if(--n) return
+
+    a.set('key', null)
+    t.equal(a.get('key'), null)
+    t.deepEqual(a.toJSON(), {})
+
     t.end()
   }
 
+a.set('key', expected.key)
+
+
 })
+
